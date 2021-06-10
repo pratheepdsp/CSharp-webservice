5f6e02586cfc6aab33e207dcbc949e6c5ca4187d
pratheep <85664860+pratheepdsp@users.noreply.github.com>
2021-06-10 11:34:48 +0530

    Add files via upload

---
+++ App_Code/BAL.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for BAL
+/// </summary>
+public class BAL
+{
+    private string _atrip;
+    private string _afrom;
+    private string _ato;
+    private string _adepart;
+    private string _aarr;
+    private string _atime;
+    private string _atimes;
+    private string _akid;
+    private string _aadult;
+    private string _aseni;
+
+
+
+
+
+
+    public int id { get; set; }
+
+    public string TRIP
+    {
+        get { return _atrip; }
+        set { _atrip = value; }
+    }
+
+    public string FROM
+    {
+        get { return _afrom; }
+        set { _afrom = value; }
+    }
+
+
+    
+
+    public string TO
+    {
+        get { return _ato; }
+        set { _ato = value; }
+    }
+
+    public string DEPART
+    {
+        get { return _adepart; }
+        set { _adepart = value; }
+    }
+
+    public string ARR
+    {
+        get { return _aarr; }
+        set { _aarr = value; }
+    }
+
+    public string TIME
+    {
+        get { return _atime; }
+        set { _atime = value; }
+    }
+    public string TIMES
+    {
+        get { return _atimes; }
+        set { _atimes = value; }
+    }
+
+    public string KID
+    {
+        get { return _akid; }
+        set { _akid = value; }
+    }
+
+    public string ADULT
+    {
+        get { return _aadult; }
+        set { _aadult = value; }
+    }
+
+    public string SENI
+    {
+        get { return _aseni; }
+        set { _aseni = value; }
+    }
+
+
+
+    public BAL()
+    {
+    }
+
+    string conStr = ConfigurationManager.ConnectionStrings["DBCon"].ConnectionString;
+
+    public string Insert()
+    {
+        using (MySqlConnection conn = new MySqlConnection(conStr))
+        {
+            MySqlCommand sqlcmd = new MySqlCommand("insertb", conn);
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+
+            sqlcmd.Parameters.AddWithValue("@p_trip", TRIP);
+            sqlcmd.Parameters.AddWithValue("@p_from", FROM);
+            sqlcmd.Parameters.AddWithValue("@p_to", TO);
+            sqlcmd.Parameters.AddWithValue("@p_depart", DEPART);
+            sqlcmd.Parameters.AddWithValue("@p_arr", ARR);
+            sqlcmd.Parameters.AddWithValue("@p_time", TIME);
+            sqlcmd.Parameters.AddWithValue("@p_times", TIMES);
+            sqlcmd.Parameters.AddWithValue("@p_kid", KID);
+            sqlcmd.Parameters.AddWithValue("@p_adult", ADULT);
+            sqlcmd.Parameters.AddWithValue("@p_senior", SENI);
+
+
+
+
+            conn.Open();
+            int count = sqlcmd.ExecuteNonQuery();
+
+            if (count > 0)
+                return "PASS";
+            else
+                return "FAIL";
+        }
+    }
+   
+
+    
+    public List<BAL> Select(int id)
+    {
+        using (MySqlConnection conn = new MySqlConnection(conStr))
+        {
+            MySqlCommand cmd = new MySqlCommand("Select_p", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@p_id", id);
+
+            conn.Open();
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            List<BAL> lstbal = new List<BAL>();
+            while (dr.Read())
+            {
+                BAL bal = new BAL
+                {
+                id = Convert.ToInt16(dr["id"]),
+                TRIP = dr["trip"].ToString(),
+                FROM = dr["fro"].ToString(),
+                TO = dr["oto"].ToString(),
+                DEPART = dr["depart"].ToString(),
+                ARR = dr["arr"].ToString(),
+                TIME = dr["time"].ToString(),
+                TIMES = dr["times"].ToString(),
+                KID = dr["kid"].ToString(),
+                ADULT = dr["adult"].ToString(),
+               SENI = dr["senior"].ToString()
+            };
+            lstbal.Add(bal);
+        }
+
+        return lstbal;
+    }
+}
+    
+
+
+
+
+
+
+    }
+
+
+      
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+    
---
+++ App_Code/WebService.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService
+{
+    public WebService()
+    {
+    }
+
+    BAL bal;
+
+    [WebMethod]
+    public string InsertEmployee(string atrip, string afrom, string ato, string adepart, string aarr, string atime, string atimes, string akid, string aadult, string aseni)
+    {
+        Common(atrip, afrom,ato,adepart,aarr,atime,atimes,akid,aadult,aseni);
+        string status = bal.Insert();
+
+        if (status == "PASS")
+            return "Ticket Booked";
+        else
+            return "Ticket Not Booked";
+    }
+   
+
+   
+
+    [WebMethod]
+    
+    public List<BAL> SelectEmployee(int id)
+    {
+        bal = new BAL();
+        List<BAL> lstbal = bal.Select(id);
+
+        if (lstbal != null)
+            return lstbal;
+        else
+            return lstbal;
+    }
+    string conStr = ConfigurationManager.ConnectionStrings["DBCon"].ConnectionString;
+
+
+
+    [WebMethod]
+    public List<BAL> AllEmployeee()
+    {
+        using (MySqlConnection conn = new MySqlConnection(conStr))
+        {
+            MySqlCommand cmd = new MySqlCommand("allrecord", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            MySqlDataAdapter da = new MySqlDataAdapter();
+            DataTable dt = new DataTable();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            List<BAL> Employee = new List<BAL>();
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    BAL empObj = new BAL();
+                    empObj.id = Convert.ToInt16(dr["Id"]);
+                    empObj.TRIP = dr["trip"].ToString();
+                    empObj.FROM= dr["fro"].ToString();
+                    empObj.TO = dr["oto"].ToString();
+                    empObj.DEPART = dr["depart"].ToString();
+                    empObj.ARR= dr["arr"].ToString();
+                    empObj.TIME = dr["time"].ToString();
+                    empObj.TIMES = dr["times"].ToString();
+                    empObj.KID = dr["kid"].ToString();
+                    empObj.ADULT = dr["adult"].ToString();
+                    empObj.SENI = dr["senior"].ToString();
+                    Employee.Add(empObj);
+
+                }
+            }
+            return Employee;
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+
+
+    private void Common(string atrip, string afrom, string ato, string adepart, string aarr, string atime, string atimes, string akid, string aadult, string aseni)
+    {
+        bal = new BAL
+        {
+            TRIP = atrip,
+            FROM = afrom,
+            TO = ato,
+             
+            DEPART  = adepart,
+            ARR = aarr,
+            TIME = atime,
+            TIMES = atimes,
+            KID = akid,
+            ADULT = aadult,
+            SENI = aseni
+        };
+    }
+
+
+
+
+}
